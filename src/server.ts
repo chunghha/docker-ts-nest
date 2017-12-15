dba3852a5b800f7eed6315d913bc701f0eff744f
Chris Ha <chunghha@users.noreply.github.com>
2017-12-14 18:52:53 -0600

    fix: host at /docs

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
                       .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('/api', app, document);
+  SwaggerModule.setup('/docs', app, document);
 
   await app.listen(3000);
 }
