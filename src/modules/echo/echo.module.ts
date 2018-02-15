6501fb703a1ac83c61cce2e81b8526aa117e3074
Chris Ha <chunghha@users.noreply.github.com>
2018-02-14 23:37:54 -0600

    feat: add middlewares

--- src/modules/echo/echo.module.ts
+++ src/modules/echo/echo.module.ts
@@ -1,7 +1,38 @@
-import {Module} from '@nestjs/common';
+import {MiddlewaresConsumer, Module} from '@nestjs/common';
+
+import {HelmetMiddleware} from '@nest-middlewares/helmet';
+import {IHelmetConfiguration} from 'helmet';
+import {ResponseTimeMiddleware} from '@nest-middlewares/response-time';
+import {ResponseTimeOptions} from 'response-time';
+
+
 import {EchoController} from './echo.controller';
 
 @Module({
   controllers: [EchoController],
 })
-export class EchoModule {}
+export class EchoModule {
+
+  configure(consumer: MiddlewaresConsumer) {
+    HelmetMiddleware.configure(this.getHelmetConfiguration());
+    ResponseTimeMiddleware.configure(this.getResponseTimeOptions());
+    consumer.apply(HelmetMiddleware).forRoutes(EchoController);
+    consumer.apply(ResponseTimeMiddleware).forRoutes(EchoController);
+  }
+
+  private getHelmetConfiguration(): IHelmetConfiguration {
+    const helmetConfiguration: IHelmetConfiguration = {
+      // default helmet configuration
+    }
+
+    return helmetConfiguration;
+  }
+
+  private getResponseTimeOptions(): ResponseTimeOptions {
+    const responseTimeOptions: ResponseTimeOptions = {
+      // default response-time options
+    }
+
+    return responseTimeOptions;
+  }
+}
