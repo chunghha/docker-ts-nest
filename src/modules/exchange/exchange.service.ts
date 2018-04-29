9c2244e2b640411df6f5fd6659bcf6da0d08ebed
Chris Ha <chunghha@users.noreply.github.com>
2018-04-28 20:16:32 -0500

    build(lib): migrate rxjs to 6

--- src/modules/exchange/exchange.service.ts
+++ src/modules/exchange/exchange.service.ts
@@ -2,7 +2,7 @@ import {Component} from '@nestjs/common';
 import {HttpService} from '@nestjs/common/http';
 import {AxiosResponse} from '@nestjs/common/http/interfaces/axios.interfaces';
 
-import {Observable} from 'rxjs/Observable';
+import {Observable} from 'rxjs';
 
 @Component()
 export class ExchangeService {
